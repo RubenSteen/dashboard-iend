c61cddf98e3427303c9afe57facb4bfca13ba7df
Ruben Steen <rubensteen123@gmail.com>
2025-02-17 19:41:10 +0100

    2 cards

--- resources/views/livewire/wallboard.blade.php
+++ resources/views/livewire/wallboard.blade.php
@@ -12,5 +12,17 @@
                 </div>
             </div>
         </div>
+        <div class="p-6 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-200">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                    <div class="flex items-center justify-center w-12 h-12 bg-blue-500 rounded-full">
+                        <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                        </svg>
+                    </div>
+                    <span class="ml-4 text-lg font-semibold">Total Open Incidents: {{ $totalOpenIncidents }}</span>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
